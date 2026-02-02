bf5b94d8d9b9ee18509fd124f1b9881b27da31fa
GrzegorzWirtek <grzegorz.wirtek@wp.pl>
2026-02-02 21:45:59 +0100

    update data

--- js/store/data.js
+++ js/store/data.js
@@ -1,43 +1,1077 @@
 export const items = [
 	{
-		name: 'AKB-567003201',
-		location: 'S2',
-		id: 'pod-sc-1',
-	},
-	{
-		name: 'NOKIA oryg',
-		location:
-			'Podłoga po prawej, na środku, przy ścianie, wzdłóż środka, albo jeszcze lepiej po lewej piszę coś sobie',
-		id: 'pod-sc-2',
-	},
-	{
-		name: 'SHW-1030 ch',
-		location: 'G2',
+		name: '0301 oryg',
+		location: 'Podłoga przy ścianie',
 		id: 'pod-sc-3',
 	},
 	{
-		name: 'AKB-12068',
-		location: 'A1',
+		name: '0467 oryg',
+		location: 'D4',
+		id: 'd',
+	},
+	{
+		name: '0477 oryg',
+		location: 'D4',
+		id: 'd',
+	},
+	{
+		name: '2454 ch',
+		location: 'H3',
+		id: 'h',
+	},
+	{
+		name: '3112 ch',
+		location: 'R2',
+		id: 'r',
+	},
+	{
+		name: '9443 oryg',
+		location: 'Murek',
+		id: 'pod-sc-5',
+	},
+	{
+		name: 'AA…0581A oryg',
+		location: 'S3',
+		id: 's',
+	},
+	{
+		name: 'AA…786A oryg',
+		location: 'U2',
+		id: 'u',
+	},
+	{
+		name: 'AA…790A oryg',
+		location: 'AA2',
+		id: 'aa',
+	},
+	{
+		name: 'AA 582 oryg',
+		location: 'Podłoga przy ścianie',
 		id: 'pod-sc-4',
 	},
 	{
-		name: 'BN-50622320',
-		location: 'Podłoga na środku',
-		id: 'pod-sr-2',
+		name: 'AA59-00602A oryg',
+		location: 'Podłoga przy ścianie',
+		id: 'pod-sc-4',
 	},
 	{
-		name: 'AKB-155550',
+		name: 'AA59-00741 oryg',
+		location: 'Podłoga przy ścianie, obok krzesła',
+		id: 'pod-sc-4',
+	},
+	{
+		name: 'AA59-00790 oryg',
+		location: 'AA1',
+		id: 'aa',
+	},
+	{
+		name: 'AKB…565 ch',
+		location: 'I3',
+		id: 'i',
+	},
+	{
+		name: 'AKB…14208 oryg',
+		location: 'U2',
+		id: 'u',
+	},
+	{
+		name: 'AKB…15303 oryg',
+		location: 'U2',
+		id: 'u',
+	},
+	{
+		name: 'AKB…15379 oryg',
+		location: 'C3',
+		id: 'c',
+	},
+	{
+		name: 'AKB…15603 oryg',
+		location: 'T3',
+		id: 't',
+	},
+	{
+		name: 'AKB…301 ch',
+		location: 'I5',
+		id: 'i',
+	},
+	{
+		name: 'AKB…308 oryg',
+		location: 'U2',
+		id: 'u',
+	},
+	{
+		name: 'AKB…324 oryg',
+		location: 'T2',
+		id: 't',
+	},
+	{
+		name: 'AKB…37605 oryg',
+		location: 'Podłoga przy ścianie',
+		id: 'pod-sc-2',
+	},
+	{
+		name: 'AKB…56504 oryg',
+		location: 'W3',
+		id: 'w',
+	},
+	{
+		name: 'AKB…56565 oryg',
+		location: 'Y2',
+		id: 'y',
+	},
+	{
+		name: 'AKB…694 oryg',
+		location: 'J1',
+		id: 'j',
+	},
+	{
+		name: 'AKB…75311 oryg',
+		location: 'B3',
+		id: 'b',
+	},
+	{
+		name: 'AKB…15324 oryg',
+		location: 'S2',
+		id: 's',
+	},
+	{
+		name: 'AKB…15601 oryg',
+		location: 'T2',
+		id: 't',
+	},
+	{
+		name: 'AKB…311 oryg',
+		location: 'B3',
+		id: 'b',
+	},
+	{
+		name: 'AKB…40…301 oryg',
+		location: '### Podłoga przy ścianie',
+		id: 'pod-sc-1',
+	},
+	{
+		name: 'AKB739757229 oryg',
+		location: 'T1',
+		id: 't',
+	},
+	{
+		name: 'AKB74115502 oryg',
+		location: 'A4',
+		id: 'a',
+	},
+	{
+		name: 'ALLENTE oryg',
+		location: 'E5',
+		id: 'e',
+	},
+	{
+		name: 'Amazon stick V2, V3, V5 ch',
+		location: 'Z1',
+		id: 'z',
+	},
+	{
+		name: 'MR20GA oryg',
+		location: 'Podłoga po lewej',
+		id: 'pod-lew',
+	},
+	{
+		name: 'MR21GA oryg',
+		location: 'Podłoga po lewej',
+		id: 'pod-lew',
+	},
+	{
+		name: 'MR22GA oryg',
+		location: 'Podłoga po lewej',
+		id: 'pod-lew',
+	},
+	{
+		name: 'AYA ch',
+		location: 'G1',
+		id: 'g',
+	},
+	{
+		name: 'BLF 0008 oryg (Blaupunkt netflix) ',
+		location: 'T2',
+		id: 't',
+	},
+	{
+		name: 'BN59-01054 oryg',
 		location: 'A1',
 		id: 'a',
 	},
 	{
-		name: 'BN-59 155550',
-		location: 'K1',
+		name: 'BN59-01175N ch',
+		location: 'I5',
+		id: 'i',
+	},
+	{
+		name: 'BN59-01178B ch',
+		location: '### Podłoga po prawej',
+		id: 'pod-sr-1',
+	},
+	{
+		name: 'BN59-01198Q',
+		location: 'Podłoga korytarz',
+		id: 'pod-kor-2',
+	},
+	{
+		name: 'BN59-01199G oryg',
+		location: 'Podłoga przy ścianie',
+		id: 'pod-sc-3',
+	},
+	{
+		name: 'BN59-01242 ch',
+		location: 'E2',
+		id: 'e',
+	},
+	{
+		name: 'BN59-01242 oryg',
+		location: 'E4',
+		id: 'e',
+	},
+	{
+		name: 'BN59-01247 oryg',
+		location: 'F4',
+		id: 'f',
+	},
+	{
+		name: 'BN59-01266 oryg',
+		location: 'E2',
+		id: 'e',
+	},
+	{
+		name: 'BN59-01266 ch',
+		location: 'G2 (lub R2)',
+		id: 'g',
+	},
+	{
+		name: 'BN59-01268 oryg',
+		location: 'U3',
+		id: 'u',
+	},
+	{
+		name: 'BN59-01278 (1290)',
+		location: 'Podłoga przy wejściu',
+		id: 'pod-sc-1',
+	},
+	{
+		name: 'BN59-01298 ch',
+		location: 'J2',
+		id: 'j',
+	},
+	{
+		name: 'BN59-01298 (1274) oryg',
+		location: 'Podłoga środek, na końcu',
+		id: 'pod-kor-3',
+	},
+	{
+		name: 'BN5901303 oryg',
+		location: 'U2',
+		id: 'u',
+	},
+	{
+		name: 'BN59-01312b (1330) ch',
+		location: 'R2',
+		id: 'r',
+	},
+	{
+		name: 'BN59-01315 oryg',
+		location: 'U3',
+		id: 'u',
+	},
+	{
+		name: 'BN59-01327 oryg',
+		location: 'W torbie używane,  srebrne z napisem ""123""',
+		id: 'pod-sc-3',
+	},
+	{
+		name: 'BN59-01328A ch',
+		location: 'A3',
+		id: 'a',
+	},
+	{
+		name: 'BN59-013330b oryg',
+		location: 'T3',
+		id: 't',
+	},
+	{
+		name: 'BN59-01357 oryg',
+		location: 'F2 (lub F3)',
+		id: 'f',
+	},
+	{
+		name: 'BN59-01357 solar voice ch',
+		location: 'Podłoga środek',
+		id: 'pod-sr-2',
+	},
+	{
+		name: 'BN59-01358b oryg',
+		location: 'AA2',
+		id: 'aa',
+	},
+	{
+		name: 'BN59-01358c oryg',
+		location: '###',
+		id: '',
+	},
+	{
+		name: 'BN59-01363 voice ch',
+		location: 'D2',
+		id: 'd',
+	},
+	{
+		name: 'BN59-01385 oryg',
+		location: 'E3',
+		id: 'e',
+	},
+	{
+		name: 'BN59-01385 solar voice ch',
+		location: 'Podłoga środek',
+		id: 'pod-sr-2',
+	},
+	{
+		name: 'BN59-01385 voice ch',
+		location: 'J2',
+		id: 'j',
+	},
+	{
+		name: 'BN59-01432 oryg',
+		location: 'Podłoga przy wejściu',
+		id: 'pod-sc-1',
+	},
+	{
+		name: 'BN59-01432J solar voice ch',
+		location: 'Z1',
+		id: 'z',
+	},
+	{
+		name: 'BN59-01432J  voice ch',
+		location: 'Z1',
+		id: 'z',
+	},
+	{
+		name: 'BN59-01455E oryg',
+		location: 'F2',
+		id: 'f',
+	},
+	{
+		name: 'BN59-01480L oryg',
+		location: 'F2',
+		id: 'f',
+	},
+	{
+		name: 'BRANDT 963 oryg',
+		location: 'F3',
+		id: 'f',
+	},
+	{
+		name: 'Chiq bez głosu oryg',
+		location: 'Podłoga naprzeciwko drzwi po lewej',
+		id: 'pod-lew',
+	},
+	{
+		name: 'Chiq NOWSZY ch',
+		location: 'H4',
+		id: 'h',
+	},
+	{
+		name: 'CMX ch',
+		location: 'H4',
+		id: 'h',
+	},
+	{
+		name: 'COCOAA oryg',
+		location: 'W2',
+		id: 'w',
+	},
+	{
+		name: 'DIGIHOME',
+		location: 'Z barkodem i bez (obok regału K)',
+		id: 'pod-sc-reg',
+	},
+	{
+		name: 'Dual 963',
+		location: 'C3',
+		id: 'c',
+	},
+	{
+		name: 'DYON ch',
+		location: 'J5',
+		id: 'j',
+	},
+	{
+		name: 'DYON oryg',
+		location: 'D2',
+		id: 'd',
+	},
+	{
+		name: 'EDEN ch',
+		location: 'J3',
+		id: 'j',
+	},
+	{
+		name: 'EN2B027H oryg',
+		location: 'U3',
+		id: 'u',
+	},
+	{
+		name: 'EN2G30 oryg',
+		location: 'T3',
+		id: 't',
+	},
+	{
+		name: 'ERF6A62 oryg',
+		location: 'T2',
+		id: 't',
+	},
+	{
+		name: 'GB028WJSA oryg',
+		location: 'A1',
+		id: 'a',
+	},
+	{
+		name: 'GB042 ch',
+		location: 'H2',
+		id: 'h',
+	},
+	{
+		name: 'GB067 ch',
+		location: 'J3',
+		id: 'j',
+	},
+	{
+		name: 'GJ220 oryg',
+		location: 'G2 (lub G3)',
+		id: 'g',
+	},
+	{
+		name: 'GRANDIN 2W1 CH V1',
+		location: 'G5',
+		id: 'g',
+	},
+	{
+		name: 'Grundig MHS oryg',
+		location: 'R3',
+		id: 'r',
+	},
+	{
+		name: 'Grundig Netflix oryg',
+		location: 'R3',
+		id: 'r',
+	},
+	{
+		name: 'HITACHI RC46160 oryg',
+		location: 'E1',
+		id: 'e',
+	},
+	{
+		name: 'HKC ch',
+		location: 'R2',
+		id: 'r',
+	},
+	{
+		name: 'HR45b oryg',
+		location: 'Podłoga przy wejściu, na górze',
+		id: 'pod-sc-1',
+	},
+	{
+		name: 'Huayu różne modele',
+		location: 'R1',
+		id: 'r',
+	},
+	{
+		name: 'HYNDAI oryg DYON',
+		location: 'I1',
+		id: 'i',
+	},
+	{
+		name: 'INFINITION ch',
+		location: 'I3',
+		id: 'i',
+	},
+	{
+		name: 'J1300',
+		location: 'Z3',
+		id: 'z',
+	},
+	{
+		name: 'J1500',
+		location: 'Z3',
+		id: 'z',
+	},
+	{
+		name: 'JBL oryg',
+		location: 'A3',
+		id: 'a',
+	},
+	{
+		name: 'Kiano 997',
+		location: 'C3',
+		id: 'c',
+	},
+	{
+		name: 'Kwadratowy Polaroid ch',
+		location: 'Podłoga naprzeciwko drzwi po prawej',
+		id: 'pod-sc-reg',
+	},
+	{
+		name: 'Mały Blaupunkt ch',
+		location: 'J4',
+		id: 'j',
+	},
+	{
+		name: 'Manta 925',
+		location: 'C3',
+		id: 'c',
+	},
+	{
+		name: 'METZ głosowy ch',
+		location: 'D2',
+		id: 'd',
+	},
+	{
+		name: 'METZ oryg',
+		location: 'D2 (lub E3, G1)',
+		id: 'd',
+	},
+	{
+		name: 'MR18BA oryg',
+		location: 'Podłoga po lewej',
+		id: 'pod-lew',
+	},
+	{
+		name: 'MR19BA oryg',
+		location: 'Podłoga po lewej',
+		id: 'pod-lew',
+	},
+	{
+		name: 'MR20GA ch',
+		location: 'Podłoga środek',
+		id: 'pod-sr-1',
+	},
+	{
+		name: 'MR21GA ch',
+		location: 'S2',
+		id: 's',
+	},
+	{
+		name: 'MR22GA ch',
+		location: 'AA2',
+		id: 'aa',
+	},
+	{
+		name: 'MR22GA oryg',
+		location: 'Podłoga po lewej',
+		id: 'pod-lew',
+	},
+	{
+		name: 'MR23GA ch',
+		location: 'AA3',
+		id: 'aa',
+	},
+	{
+		name: 'MR23GA oryg',
+		location: 'Podłoga po lewej',
+		id: 'pod-lew',
+	},
+	{
+		name: 'MR24GA ch',
+		location: 'I4',
+		id: 'i',
+	},
+	{
+		name: 'MR24GA oryg',
+		location: 'Podłoga po lewej',
+		id: 'pod-lew',
+	},
+	{
+		name: 'MR600 ch',
+		location: 'A3',
+		id: 'a',
+	},
+	{
+		name: 'MR 650A ch usb',
+		location: 'S1 z tyłu',
+		id: 's',
+	},
+	{
+		name: 'MR650A oryg',
+		location: 'D2',
+		id: 'd',
+	},
+	{
+		name: 'MR650A voice ch',
+		location: 'B3',
+		id: 'b',
+	},
+	{
+		name: 'N2Q …0829 oryg',
+		location: 'B4',
+		id: 'b',
+	},
+	{
+		name: 'N2Q …0863 oryg',
+		location: 'B4',
+		id: 'b',
+	},
+	{
+		name: 'N2Q …1333 oryg',
+		location: 'Y3',
+		id: 'y',
+	},
+	{
+		name: 'N2Q …1212 (1211) oryg',
+		location: 'Y3',
+		id: 'y',
+	},
+	{
+		name: 'N2Q …0051 oryg',
+		location: 'Podłoga środek',
+		id: 'pod-sr-1',
+	},
+	{
+		name: 'N2Q …0056 oryg',
+		location: '###Podłoga przy ścianie',
+		id: 'pod-sc-3',
+	},
+	{
+		name: 'N2Q …0060 oryg',
+		location: 'Podłoga przy wejściu',
+		id: 'pod-sc-1',
+	},
+	{
+		name: 'N2Q …0830 oryg',
+		location: 'B4',
+		id: 'b',
+	},
+	{
+		name: 'N2Q …1010 oryg',
+		location: 'F5',
+		id: 'f',
+	},
+	{
+		name: 'Nokia oryg',
+		location: 'T1',
+		id: 't',
+	},
+	{
+		name: 'Nokia RC110 oryg',
+		location: 'B1',
+		id: 'b',
+	},
+	{
+		name: 'ONVO oryg',
+		location: 'Y2',
+		id: 'y',
+	},
+	{
+		name: 'ORION z DVD ch',
+		location: 'J4',
+		id: 'j',
+	},
+	{
+		name: 'Panasonic RC48127 oryg',
+		location: 'B1',
+		id: 'b',
+	},
+	{
+		name: 'Philips Netflix ch',
+		location: 'H5',
+		id: 'h',
+	},
+	{
+		name: 'Philips Rakuten Ambilight oryg',
+		location: 'Podłoga przy ścianie',
+		id: 'pod-sc-2',
+	},
+	{
+		name: 'Philips Rakuten ch',
+		location: 'Podłoga przy ścianie, koniec',
+		id: 'pod-sc-4',
+	},
+	{
+		name: 'Philips Rakuten oryg',
+		location: 'Podłoga przy ścianie, koniec',
+		id: 'pod-sc-4',
+	},
+	{
+		name: 'RC1818 / RC4592',
+		location: 'T3 tył (S3)',
+		id: 't',
+	},
+	{
+		name: 'RC3902 ch',
+		location: 'H2',
+		id: 'h',
+	},
+	{
+		name: 'RC43141',
+		location: 'U2',
+		id: 'u',
+	},
+	{
+		name: 'RC4861 ch',
+		location: 'J5',
+		id: 'j',
+	},
+	{
+		name: 'RC4875 ch',
+		location: 'J4',
+		id: 'j',
+	},
+	{
+		name: 'RC5117 ch',
+		location: 'R2',
+		id: 'r',
+	},
+	{
+		name: 'RC802N oryg',
+		location: 'S3',
+		id: 's',
+	},
+	{
+		name: 'RC4154403 voice ch',
+		location: 'K2',
 		id: 'k',
 	},
 	{
-		name: 'BN-59 459900',
-		location: 'G',
+		name: 'RC833 oryg',
+		location: '###Podłoga środek',
+		id: 'pod-sr-2',
+	},
+	{
+		name: 'RC901V oryg',
+		location: 'U3',
+		id: 'u',
+	},
+	{
+		name: 'RCA ch',
+		location: 'R2 ',
+		id: 'r',
+	},
+	{
+		name: 'RCA49128 oryg',
+		location: 'W2',
+		id: 'w',
+	},
+	{
+		name: 'RCA4995 ch',
+		location: 'R2',
+		id: 'r',
+	},
+	{
+		name: 'RM-ED0008/007',
+		location: 'A1',
+		id: 'a',
+	},
+	{
+		name: 'RM-ED047 oryg',
+		location: 'S3',
+		id: 's',
+	},
+	{
+		name: 'RM-ED052 oryg',
+		location: 'Podłoga środek',
+		id: 'pod-sr-2',
+	},
+	{
+		name: 'RM-ED053 oryg',
+		location: 'Podłoga środek',
+		id: 'pod-sr-2',
+	},
+	{
+		name: 'RM-ED060 oryg',
+		location: 'Podłoga środek',
+		id: 'pod-kor-3',
+	},
+	{
+		name: 'RM-ED061 oryg',
+		location: 'Podłoga środek',
+		id: 'pod-kor-3',
+	},
+	{
+		name: 'RMF-TX300E voice ch',
+		location: 'D1',
+		id: 'd',
+	},
+	{
+		name: 'RMF-TX301E oryg',
+		location: 'I4',
+		id: 'i',
+	},
+	{
+		name: 'RMF-TX500E voice ch',
+		location: 'D3',
+		id: 'd',
+	},
+	{
+		name: 'RMF-TX520E oryg',
+		location: '###',
+		id: '',
+	},
+	{
+		name: 'RMF-TX520E voice ch',
+		location: 'U1',
+		id: 'u',
+	},
+	{
+		name: 'RMF-TX600E oryg',
+		location: 'B2',
+		id: 'b',
+	},
+	{
+		name: 'RMF-TX611E ch',
+		location: 'T1',
+		id: 't',
+	},
+	{
+		name: 'RMF-TX611E oryg',
+		location: 'B2',
+		id: 'b',
+	},
+	{
+		name: 'RMF-TX621E ch',
+		location: 'U1',
+		id: 'u',
+	},
+	{
+		name: 'RMF-TX800U voice ch',
+		location: 'AA1',
+		id: 'aa',
+	},
+	{
+		name: 'RMF-TX900U ch / 910Uch',
+		location: 'Z1',
+		id: 'z',
+	},
+	{
+		name: 'RM-L1030',
+		location: 'R2',
+		id: 'r',
+	},
+	{
+		name: 'RM-L1220',
+		location: 'S1',
+		id: 's',
+	},
+	{
+		name: 'RM-L1611',
+		location: 'S1',
+		id: 's',
+	},
+	{
+		name: 'RMT-TX100D oryg',
+		location: 'C2',
+		id: 'c',
+	},
+	{
+		name: 'RMT-TX102D oryg',
+		location: 'A2',
+		id: 'a',
+	},
+	{
+		name: 'RMT-TX200D oryg',
+		location: 'C4',
+		id: 'c',
+	},
+	{
+		name: 'RMT-TX300D oryg',
+		location: 'C1',
+		id: 'c',
+	},
+	{
+		name: 'SABA 2w1 V1 ch',
+		location: 'H5',
+		id: 'h',
+	},
+	{
+		name: 'Schaub nowy',
+		location: 'C4 (lub D3)',
+		id: 'c',
+	},
+	{
+		name: 'Schneider 983',
+		location: 'D3',
+		id: 'd',
+	},
+	{
+		name: 'SHARP Z MEDIA oryg',
+		location: 'B2',
+		id: 'b',
+	},
+	{
+		name: 'SHW/RMC/0103oryg',
+		location: 'I1 (lub I2)',
+		id: 'i',
+	},
+	{
+		name: 'SHW/RMC/0115 ch',
+		location: 'G4',
 		id: 'g',
+	},
+	{
+		name: 'SHW/RMC/0115 oryg',
+		location: 'A2',
+		id: 'a',
+	},
+	{
+		name: 'SHW/RMC/0117 oryg',
+		location: 'E5',
+		id: 'e',
+	},
+	{
+		name: 'SHW/RMC/0134 ch',
+		location: 'S2',
+		id: 's',
+	},
+	{
+		name: 'SHW/RMC/0134 oryg',
+		location: 'A2',
+		id: 'a',
+	},
+	{
+		name: 'SHW/RMC/0134 voice ch (0133)',
+		location: 'S1',
+		id: 's',
+	},
+	{
+		name: 'SHW/RMC/0138N oryg',
+		location: 'A2',
+		id: 'a',
+	},
+	{
+		name: 'SKYWORTH SREBRNY ch',
+		location: 'G4',
+		id: 'g',
+	},
+	{
+		name: 'SKYWORTH voice ch',
+		location: 'T1',
+		id: 't',
+	},
+	{
+		name: 'Smart-tech ch',
+		location: 'Podłoga środek od regałów',
+		id: 'pod-sr-reg-2',
+	},
+	{
+		name: 'SRC-4526 oryg',
+		location: '###Podłoga po prawej blisko ściany',
+		id: 'pod-sc-3',
+	},
+	{
+		name: 'STRONG oryg',
+		location: 'E2',
+		id: 'e',
+	},
+	{
+		name: 'SHW/0134 voice ch',
+		location: 'D2',
+		id: 'd',
+	},
+	{
+		name: 'TD SYSTEM MAŁY ch',
+		location: 'G4',
+		id: 'g',
+	},
+	{
+		name: 'TELESYSTEM oryg',
+		location: 'E4',
+		id: 'e',
+	},
+	{
+		name: 'TESLA ch',
+		location: 'H5 (lub J1)',
+		id: 'h',
+	},
+	{
+		name: 'THOMSON RC902V oryg',
+		location: 'A4',
+		id: 'a',
+	},
+	{
+		name: 'Toshiba CT-8564 oryg',
+		location: 'B1',
+		id: 'b',
+	},
+	{
+		name: 'TUSCON ch',
+		location: 'H2',
+		id: 'h',
+	},
+	{
+		name: 'TV-D38 ch',
+		location: 'H4',
+		id: 'h',
+	},
+	{
+		name: 'TV-D44',
+		location: 'C4',
+		id: 'c',
+	},
+	{
+		name: 'TV-D45',
+		location: 'C4',
+		id: 'c',
+	},
+	{
+		name: 'VIVAX ch',
+		location: 'H3',
+		id: 'h',
+	},
+	{
+		name: 'WZ358 oryg',
+		location: 'H1',
+		id: 'h',
+	},
+	{
+		name: 'XMRM 002 oryg',
+		location: 'E1',
+		id: 'e',
+	},
+	{
+		name: 'XM-RM006 V1 ch',
+		location: 'S1',
+		id: 's',
+	},
+	{
+		name: 'XM-RM006 V2 ch',
+		location: 'G1',
+		id: 'g',
+	},
+	{
+		name: 'XM-RM010 ch',
+		location: 'S1',
+		id: 's',
+	},
+	{
+		name: 'XM-RM019 ch',
+		location: 'G3',
+		id: 'g',
+	},
+	{
+		name: 'XM-RM019 oryg',
+		location: 'S1',
+		id: 's',
+	},
+	{
+		name: 'YKF406-003 (Philips netflix oryg)',
+		location: 'D4',
+		id: 'd',
+	},
+	{
+		name: 'YKF463-B010 oryg',
+		location: 'Podłoga przy ścianie',
+		id: 'pod-sc-1',
+	},
+	{
+		name: 'YKF474 oryg',
+		location: 'Podłoga przy ścianie',
+		id: 'pod-sc-2',
+	},
+	{
+		name: 'YKF474 voice ch',
+		location: 'D1',
+		id: 'd',
 	},
 ];
