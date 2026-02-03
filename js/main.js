fb471c1ba5ae809fd58cd8e8f1d5c303d85b2f19
GrzegorzWirtek <grzegorz.wirtek@wp.pl>
2026-02-03 13:35:56 +0100

    move register service worker to index.html inline

--- js/main.js
+++ js/main.js
@@ -1,19 +1,6 @@
 import { findItems } from './search.js';
 import { viewResults } from './viewFoundItems.js';
 
-if ('serviceWorker' in navigator) {
-	window.addEventListener('load', () => {
-		navigator.serviceWorker
-			.register('../service-worker.js')
-			.then((registration) => {
-				console.log('Service Worker zarejestrowany:', registration.scope);
-			})
-			.catch((error) => {
-				console.error('Błąd rejestracji SW:', error);
-			});
-	});
-}
-
 const searchButton = document.querySelector('#search-button');
 
 const handleSubmit = (e) => {
